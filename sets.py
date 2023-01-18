d1d4b323d7289dfed2795744315134d2492a962f
Neha-Thotapalli <97404157+Neha-Thotapalli@users.noreply.github.com>
2023-01-18 11:15:06 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,8 @@
+#Sets
+#IS an unordered collction of datatype
+#sets cant containt a duplicate values
+#once the set is created the the items can be added but not modified.
+#sets are immutable , mutable at the same time . It is an unordered list , hence inedexing isnt possible .
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+string_letters = set(letters)
+print(string_letters)
